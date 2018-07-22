3569b7ebc9752b2f09f0b7ffc0385e0e57819c07
Dr Bean <drbean@sac.nuu.edu.tw>
2018-07-22 15:11:55 +0800

    s/PGF/PGF2/.remove_PGF:langs,morpho,liftOp,miss.comment_out_evaluation_test_code,s/tab/2-space/.not_working.TODO!

--- Tests.hs
+++ Tests.hs
@@ -4,10 +4,11 @@ import Control.Monad
 import Data.Maybe
 import Data.Char
 import Data.List
+import qualified Data.Map as Map
 
 import Data.DRS
 
-import PGF
+import PGF2
 import Trigger
 import Representation
 import Evaluation
@@ -20,46 +21,40 @@ import Model
 gr :: IO PGF
 gr = readPGF "./Trigger.pgf"
 
-langs :: IO [Language]
-langs = liftM languages gr
-
-lang :: IO Language
-lang = liftM head langs
-
-morpho :: IO Morpho
-morpho = liftM2 buildMorpho gr lang
-
-liftOp :: Monad m => (a -> b -> c) -> m a -> b -> m c
-liftOp f a b = a >>= \a' -> return (f a' b)
-
-miss :: [String] -> IO [String]
-miss ws =
-	liftOp morphoMissing morpho ws
-
 cat2funs :: String -> IO ()
 cat2funs cat = do
-	gr' <- gr
-	let fs = functionsByCat gr' (mkCId cat)
-	let ws = filter (isLower . head . showCId) fs
-	let is = map (reverse . dropWhile (\x ->  (==) x '_' || isUpper x) . reverse .showCId ) ws
-	putStrLn (unwords is)
+  gr' <- gr
+  let fs = functionsByCat gr' (show cat)
+  let ws = filter (isLower . head . show) fs
+  let is = map (reverse . dropWhile (\x ->  (==) x '_' || isUpper x) . reverse .show ) ws
+  putStrLn (unwords is)
 
 catByPOS :: String -> IO ()
 catByPOS  pos = do
-	gr' <- gr
-	let allCats = categories gr'
-	let cats = filter (isPrefixOf pos . showCId) allCats
-	putStrLn (unwords (map showCId cats))
+  gr' <- gr
+  let allCats = categories gr'
+  let cats = filter (isPrefixOf pos . show) allCats
+  putStrLn (unwords (map show cats))
 
 trans = id
 
 run f tests = do
-  gr	<- readPGF "./Trigger.pgf"
+  gr' <- gr
+  let Just eng = Map.lookup "TriggerEng" (languages gr')
   let ss = map (chomp . lc_first) tests
-  let ps = map ( parses gr ) ss
-  let ts = map f ps
-  let zs = zip (map (++"\t") tests) (map (map (showExpr []) ) ts)
+  let p =  parse eng (startCat gr')
+  -- let ps = map p ss
+  let Just incompleteparse = readExpr "ParseIncomplete"
+  let failingparse n string = readExpr ("ParseFailed at " ++ string ++ ": " ++ (show n))
+  let t (ParseOk ((e,prob):rest)) = e
+  let t (ParseFailed offset token) = fromMaybe incompleteparse (failingparse offset token)
+  let t ParseIncomplete = fromMaybe incompleteparse (failingparse 0 "incomplete")
+  let t _ = incompleteparse
+  let ts = map (t . p) ss
+  let zs = zip (map (++"\t") tests) ts
   putStrLn (unlines (map (\(x,y) -> x ++ (show y ) ) zs) )
+
+{-
 
 ans tests = do
   gr	<- readPGF "./Trigger.pgf"
@@ -95,37 +90,39 @@ fol tests = do
 	let zs = zip (map (++"\t") tests) ts
 	putStrLn (unlines (map (\(x,y) -> x ++ (show y ) ) zs) )
 
+-}
+
 dic_test = [
 
-	"A turning point for Sally was when she learned Spanish."
-	, "She was envious of kids who could speak another language."
-	, "When she learned Spanish, she found she was actually pretty good at it."
-	, "When she reached the breakthrough stage--when she discovered she could actually communicate with people in the language--she was really proud of herself."
-	, "Now she can speak three languages."
-	, "Henry has a twin brother and they have always been very close."
-	, "When he was 18, they went to different colleges."
-	, "That was the first time they had had to cope on their own."
-	, "Henry thinks it was good in a way, because they both became more confident and independent."
-	, "He realized he was actually capable of working things out on his own."
-	, "Debbie guesses she was always pretty shy."
-	, "She didn't share a lot with people, not even with her parents."
-	, "When she got a prize for being top student, she didn't think too much about it."
-	, "But people suddenly started treating her differently."
-	, "She thinks some of the kids in school started looking up to her."
-	, "And she became a lot more outgoing after that."
-	, "Alex stopped exercising."
-	, "He thinks it's the dumbest thing he ever did."
-	, "He has been trying to lose weight for the last year and a half, but it's really difficult."
-	, "He thought he would be thin forever and he ate junk food all the time."
-	, "But after college, he quit playing sports."
-	, "Now he is determined to join a gym, because he knows he can't get healthy by just dieting."
-	, "If YiYun had had a choice, she would have learned to play the guitar."
-	, "When she was a kid, her parents made her study piano."
-	, "She loves the piano, but wishes she could bring a guitar to a party and have everyone sing songs."
-	, "Jacob regrets not going to Europe with his friends when he had the choice."
-	, "In the summer after he graduated, he started looking for a job, but his friends went backpacking in Europe for a few weeks."
-	, "He thinks he should have gone because he didn't get a job right away and his friends had an unforgettable time together."
-	, "He regrets it because they had an amazing experience without him and he could have gone."
+  "A turning point for Sally was when she learned Spanish."
+  , "She was envious of kids who could speak another language."
+  , "When she learned Spanish, she found she was actually pretty good at it."
+  , "When she reached the breakthrough stage--when she discovered she could actually communicate with people in the language--she was really proud of herself."
+  , "Now she can speak three languages."
+  , "Henry has a twin brother and they have always been very close."
+  , "When he was 18, they went to different colleges."
+  , "That was the first time they had had to cope on their own."
+  , "Henry thinks it was good in a way, because they both became more confident and independent."
+  , "He realized he was actually capable of working things out on his own."
+  , "Debbie guesses she was always pretty shy."
+  , "She didn't share a lot with people, not even with her parents."
+  , "When she got a prize for being top student, she didn't think too much about it."
+  , "But people suddenly started treating her differently."
+  , "She thinks some of the kids in school started looking up to her."
+  , "And she became a lot more outgoing after that."
+  , "Alex stopped exercising."
+  , "He thinks it's the dumbest thing he ever did."
+  , "He has been trying to lose weight for the last year and a half, but it's really difficult."
+  , "He thought he would be thin forever and he ate junk food all the time."
+  , "But after college, he quit playing sports."
+  , "Now he is determined to join a gym, because he knows he can't get healthy by just dieting."
+  , "If YiYun had had a choice, she would have learned to play the guitar."
+  , "When she was a kid, her parents made her study piano."
+  , "She loves the piano, but wishes she could bring a guitar to a party and have everyone sing songs."
+  , "Jacob regrets not going to Europe with his friends when he had the choice."
+  , "In the summer after he graduated, he started looking for a job, but his friends went backpacking in Europe for a few weeks."
+  , "He thinks he should have gone because he didn't get a job right away and his friends had an unforgettable time together."
+  , "He regrets it because they had an amazing experience without him and he could have gone."
 
   ]
 
